c02a2cc0afa4c61247b92a2822a70e55baa26500
MD HARIS OSMAN <usmanmdharis@gmail.com>
2025-12-03 06:30:22 +0530

    Last change to OMDB

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }
   return (
     <>    
-    <div className="px-4 sm:px-6 md:px-10">
+    <div className="px-4 sm:px-6 md:px-10 mb-10">
     <div className="text-center mt-2">
     <h1 className="text-neutral-100 text-2xl sm:text-3xl font-bold mt-5">Search unlimited movies, TV shows and more</h1>
     <p className="text-neutral-300 text-lg sm:text-xl font-semibold mt-0.5">Ready to watch? Enter your movie name.</p>
@@ -30,12 +30,12 @@ function App() {
     <div className="border-b flex flex-col md:flex-row justify-between items-center border-neutral-800">
     <h1 className="font-bold text-2xl sm:text-3xl text-center mb-4 md:mb-0">OMDb API Clone</h1>
     <div className="flex flex-col md:flex-row gap-3 md:gap-2 justify-between w-full md:w-[1050px] p-5">
-      <div className="flex flex-col sm:flex-row gap-2 w-full justify-center">
-        <input type="text" ref={movieInput} placeholder="Enter movie name." className="border border-neutral-800 px-4 rounded-xl w-full sm:w-[500px] bg-neutral-800 py-2 focus:outline-2 focus:outline-blue-700"/>
-        <button onClick={movieHandler} className="bg-red-600 rounded-xl text-white font-bold px-4 py-2 w-full sm:w-auto">Submit
+      <div className="flex flex-col sm:flex-row gap-2  justify-center">
+        <input type="text" ref={movieInput} placeholder="Enter movie name." className="border border-neutral-800 px-4 rounded-xl w-full sm:w-[500px] bg-neutral-800 py-2 focus:outline-2 focus:outline-green-400"/>
+        <button onClick={movieHandler} className="bg-red-600 cursor-pointer rounded-xl text-white font-bold px-4 py-2 w-full sm:w-auto">Submit
         </button>
       </div>
-      <button className="bg-green-600 px-4 py-2 rounded-lg w-full sm:w-auto">Log-in</button>
+      <button className="bg-green-700 cursor-pointer px-4 py-2 rounded-lg w-full sm:w-auto">Log-in</button>
     </div>
     </div>
   {
